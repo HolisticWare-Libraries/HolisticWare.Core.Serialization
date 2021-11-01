8495334d0e889b1356df09fc4bee6dfa318b8826
moljac <mcvjetko@holisticware.net>
2021-11-01 18:08:21 +0100

    InfererDeducer

---
+++ source/HolisticWare.Core.SchemaDeducerInferer/IInferenceDeduction.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.Serialization
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IInferenceDeduction
+    {
+        string InferDeduceSchema(string data);
+    }
+}
