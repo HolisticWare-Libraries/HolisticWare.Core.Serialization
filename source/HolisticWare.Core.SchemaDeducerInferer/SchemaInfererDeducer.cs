fce0c1a51586a1ef4bd7ab31b75ac9a40039083b
moljac <mcvjetko@holisticware.net>
2022-01-06 23:18:37 +0100

    source

---
+++ source/HolisticWare.Core.SchemaDeducerInferer/SchemaInfererDeducer.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.Serialization.SchemaInferenceDeduction
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// https://github.com/RicoSuter/NJsonSchema
+    /// 
+    public class SchemaInfererDeducer
+                        :
+                        ISchemaInfererDeducer
+    {
+        public string InferDeduceSchema(string data)
+        {
+            string result = null;
+
+            Task<NJsonSchema.JsonSchema> schema = NJsonSchema.JsonSchema.FromJsonAsync(data);
+
+
+            NJsonSchema.CodeGeneration.CSharp.CSharpGenerator generator = null;
+            generator = new NJsonSchema.CodeGeneration.CSharp.CSharpGenerator(schema);
+
+            System.Collections.Generic.IEnumerable<NJsonSchema.CodeGeneration.CodeArtifact> types = null;
+            types = generator.GenerateTypes();
+
+
+
+            return result;
+        }
+
+
+    }
+}
