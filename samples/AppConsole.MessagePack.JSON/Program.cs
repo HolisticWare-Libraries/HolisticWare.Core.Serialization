1fccaf75e75df3d9ff3cf6a164255c149773cbf1
moljac <mcvjetko@holisticware.net>
2024-09-20 22:39:18 +0200

    Messagepack.JSON

---
+++ samples/AppConsole.MessagePack.JSON/Program.cs
@@ -0,0 +1,20 @@
+﻿using HolisticWare.MessagePack;
+
+using HolisticWare.Ph4ct3x;
+
+Person person = new()
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Age = 30
+                };
+
+// Serialize to JSON format
+string jsonData = CustomMessagePackSerializer.SerializeToJson(person);
+Console.WriteLine("Serialized to JSON: " + jsonData);
+
+// Deserialize from JSON format
+Person? deserializedFromJsonPerson = CustomMessagePackSerializer.DeserializeFromJson<Person>(jsonData);
+Console.WriteLine($"Deserialized from JSON: {deserializedFromJsonPerson.FirstName} {deserializedFromJsonPerson.LastName}, Age: {deserializedFromJsonPerson.Age}");
+
+return;
