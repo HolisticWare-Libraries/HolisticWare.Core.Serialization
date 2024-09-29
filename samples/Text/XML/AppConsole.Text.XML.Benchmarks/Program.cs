32c78fe021984820ab2a724f6f18a2dbbca3cea7
moljac <mcvjetko@holisticware.net>
2024-09-29 14:34:21 +0200

    benchmarks

---
+++ samples/Text/XML/AppConsole.Text.XML.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+please provide code sample in C# for
+   StackXml
+serialization and deserialization
+
+*/
+
+using AppConsole.Text.XML.Benchmarks;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+public class Program
+{
+   public static void Main(string[] args)
+   {
+      Summary summary_XML = BenchmarkRunner.Run<Benchmarks_XML>();
+   }
+}
