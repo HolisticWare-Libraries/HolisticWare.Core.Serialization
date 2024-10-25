ea3260cb72bff41b5a50f42763ea57cb4ceb82b9
moljac <mcvjetko@holisticware.net>
2024-10-25 22:27:32 +0200

    FlatSharp

---
+++ samples/Binary/AppConsole.Binary.FlatSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+please provide code sample in C# for
+   FlatBuffers
+serialization and deserialization
+
+https://github.com/jamescourtney/FlatSharp
+*/
+
+using System;
+using Google.FlatBuffers;
+using MyGame.Sample;
+
+class Program
+{
+   static void Main()
+   {
+      Person person = new Person(...);
+      int maxBytesNeeded = Person.Serializer.GetMaxSize(person);
+      byte[] buffer = new byte[maxBytesNeeded];
+      int bytesWritten = Person.Serializer.Serialize(buffer, person);
+   
+      Person p = Person.Serializer.Parse(data);      
+   }
+}
+
+return;
